2e0680138722f947b668eb10b1642584298f48a5
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-12-25 15:08:37 -0300

    Inicialização

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {Post} from './Post'
+
+export function App() {
+
+  return (
+    <>
+     <Post 
+      author="Robson de Jesus" 
+      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam vero commodi ad optio laudantium saepe recusandae perferendis corporis. Magnam, facilis."
+     />
+
+
+    <Post 
+      author="Fernanda de souza" 
+      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam vero commodi ad optio laudantium saepe recusandae perferendis corporis. Magnam, facilis."
+     />
+    </>
+  )
+}
+
