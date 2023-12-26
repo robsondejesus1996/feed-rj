1ac4d265b768c7b902eb2574bb8c7a23976c1784
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-12-26 15:16:52 -0300

    Componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import {Post} from './Post'
+import { Post } from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
+
 
 export function App() {
 
   return (
-    <>
+    <div>
 
-     <Header/>
-     <Post 
-      author="Robson de Jesus" 
-      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam vero commodi ad optio laudantium saepe recusandae perferendis corporis. Magnam, facilis."
-     />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author="Robson de Jesus"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa voluptatum delectus sequi recusandae fugit adipisci unde enim praesentium labore quae."
+          />
+
+          <Post
+            author="Robson de Jesus"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa voluptatum delectus sequi recusandae fugit adipisci unde enim praesentium labore quae."
+          />
+
+        </main>
+      </div>
 
 
-    <Post 
-      author="Fernanda de souza" 
-      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam vero commodi ad optio laudantium saepe recusandae perferendis corporis. Magnam, facilis."
-     />
-    </>
+
+    </div>
   )
 }
 
