715ecd6912c12fdb290589e44a49475d19828ccb
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-12-26 16:03:32 -0300

    Finalização Sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
-import igniteLogo from '../assets/ignite-logo.svg';
+import igniteLogo from '../assets/Ignite-logo.svg';
 
-console.log(igniteLogo)
 
-export function Header(){
-    return(
-        <header className={styles.header}>
-            <img src={igniteLogo} alt='Logotipo do Ignite'/>
-        </header>
-    );
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logo Feed"/>
+    </header>
+  );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,29 @@
-import styles from './Sidebar.module.css'
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            <img className={styles.cover}
-                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
-            />
-            <div className={styles.profile}>
-                <stron>Robson de Jesus</stron>
-                <span>Web Develop</span>
-            </div>
-            <footer>
-                <a href="#">
-                    Editar o seu perfil
-                </a>
-            </footer>
-        </aside>
-    )
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+
+        <Avatar src="https://github.com/robsondejesus1996.png" />
+
+        <strong> Robson de Jesus</strong>
+        <span>Sofware Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
