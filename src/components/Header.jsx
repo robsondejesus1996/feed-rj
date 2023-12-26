851251d0bb5893a992ca8a093f9dfd96653ffa20
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-12-26 14:35:36 -0300

    Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg';
 
+console.log(igniteLogo)
 
 export function Header(){
     return(
         <header className={styles.header}>
-            <strong>RJ Feed</strong>
+            <img src={igniteLogo} alt='Logotipo do Ignite'/>
         </header>
     );
 }
